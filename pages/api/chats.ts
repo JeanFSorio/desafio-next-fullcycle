2b86b18ddc06c81710261791422dd6f569db3133
Jean Felipe Sorio <jeanfsorio@gmail.com>
2023-04-22 12:52:38 -0300

    initial commit

---
+++ pages/api/chats.ts
@@ -0,0 +1,28 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from "../../prisma"
+
+type Data = {
+  name: string
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+
+    if (req.method == "POST") {
+        const { message } = req.body;
+        const chat = await prisma.chat.create({
+            data: {
+                message,
+            },
+        })
+        res.status(200).json(chat)
+    } 
+    if (req.method == "GET") {
+        const chats = await prisma.chat.findMany();
+        res.json(chats)
+    }
+
+    res.status(405).json({ message: "Not Allowed"})
+}
+
+export default handler;
